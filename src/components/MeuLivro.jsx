18fcb21cba7238b3930d8f68175c2157cc33c4c4
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-04-08 15:41:22 -0300

    adicionando imagens de projetos e criando a seção "Sobre"

--- src/components/MeuLivro.jsx
+++ src/components/MeuLivro.jsx
@@ -8,6 +8,40 @@ import Habilidades from './Paginas/Habilidades/Habilidades';
 import Serviços from './Paginas/Serviços/Serviços';
 import Sobre from './Paginas/Sobre/Sobre';
 
+import ProjetoImg1 from '../assets/Projetos/Jogo de cartas.png';
+import ProjetoImg2 from '../assets/Projetos/2048.png';
+import ProjetoImg3 from '../assets/Projetos/jogo do dino chorme.png';
+import ProjetoImg4 from '../assets/Projetos/site ecomerrce.png';
+
+const ProjectData = [
+  {
+    nome: "Ache os As",
+    descrição: "Um simples jogo de cartas onde o jogador deve encontrar o As .",
+    image: ProjetoImg1 ,
+    previewLink: "https://ache-os-as.netlify.app/",
+  },
+  {
+    nome: "Jogo 2048",
+    descrição: "Um simples jogo de 2048 onde o jogador deve juntar os números até chegar a 2048 ou mais.",
+    image: ProjetoImg2,
+    previewLink: "https://jogo2048.netlify.app/",
+    },
+]
+const ProjectData2 = [
+  {
+    nome: "Jogo do Dinossauro",
+    descrição: "Um simples jogo do dinossauro onde o jogador deve pular os obstáculos.",
+    imagem: ProjetoImg3,
+    previewLink: "https://eliseucostaalbano.github.io/Jogo-do-dino-chorme/",
+  },
+  {
+    nome: "Ecommerce website",
+    descrição: "Um simples site de ecommerce Usando React e TailWind.",
+    imagem: ProjetoImg4,
+    previewLink: "https://eliseu-ecomerrce.netlify.app/",
+  },
+];
+
 function MeuLivro() {
     return (
         <HTMLFlipBook width={600} height={700} showCover={true}>
--- src/components/Paginas/Sobre/Sobre.jsx
+++ src/components/Paginas/Sobre/Sobre.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { MdFacebook } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
 const Sobre = () => {
   return (
     <div className='h-full page-shadow bg-white px-10'>
-    <div>Sobre</div>
+      <div className='flex flex-col items-center'>
+        <h1 className='inline-block text-3xl text-center font-bold mb-3 border-b-2 border-[#150184] pb-1'>Sobre</h1> 
+        <p className='text-sm text-slate-500 text-center md: w-[80%] mx-auto'>Aqui Algumas Coisas Sobre Mim</p>
+      </div>
+
+      {/* seção de detalhes */}
+      <div className='mt-10 text-slate-500'>
+         <p>
+          Começei minha Jornada Em 2022 quando comecei a estudar HTML e CSS, depois disso fui aprendendo mais e mais sobre FrontEnd e BackEnd, hoje em dia tenho conhecimentos em HTML, CSS, JS, React, NodeJS, Express, MongoDB e SQL.
+        </p>
+        <br />
+        <p>
+          Tambem tenho conhecimentos em GameDev, com a Game Engine Da Unity, onde crio jogos 2D e 3D usando C#.
+        </p>
+         {/* Links Sociais  */}
+         <div className="flex space-x-4 mt-10">
+          <MdFacebook className='social-btn'/>
+          <FaLinkedin className='social-btn'/>
+          <FaGithub className='social-btn'/>
+         </div> 
+         {/* Butoes de link  */}
+         <div className='flex space-x-4 mt-4'>
+          <a href='#' className='primary-btn'>Curriculo</a>
+          <a href='#' className='outline-btn'>Contato</a>
+         </div>
+      </div>
     </div>
   )
 }
